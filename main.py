3cd4c6d139bdbbd804bbb5d9192845782c87e90c
abhishekc402 <163327387+abhishekc402@users.noreply.github.com>
2024-04-21 13:23:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ hide_st_style = """
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             header {visibility: hidden;}
+            div.embeddedAppMetaInfoBar_container__DxxL1 {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
